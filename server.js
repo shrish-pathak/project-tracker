aab0deb8500db6acb98f99843412daac69ca6040
_shrish_ <shrish@evoxsys.net>
2020-10-20 14:55:22 +0530

    passport setup and user route binding to express app instance in server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express()
 const mongoose = require('mongoose')
 const mongoURI = require('./config/keys').mongoURI
 
+const passport = require('passport')
+
+const users = require('./routes/users')
+
 mongoose.set('useNewUrlParser', true)
 mongoose.set('useFindAndModify', false)
 mongoose.set('useCreateIndex', true)
@@ -19,12 +23,16 @@ mongoose.connect(mongoURI).then(() => {
     process.exit(1)
 })
 
+require('./config/passport')(passport)
+
 app.get('/test', (req, res) => {
     res.send("working")
 })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use('/api/users', users)
 
 const port = process.env.PORT || 4000
 app.listen(port, () => { console.log(`server running on ${port}`) })
