fbfd06581135f5f1f1c9f7adcd47e9745adf6aaf
_shrish_ <shrish@evoxsys.net>
2020-10-21 10:00:47 +0530

    chat, message route binding to express app instance

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const passport = require('passport')
 const users = require('./routes/users')
 const projects = require('./routes/projects')
 const tasks = require('./routes/tasks')
+const chats = require('./routes/chats')
+const messages = require('./routes/messages')
 
 mongoose.set('useNewUrlParser', true)
 mongoose.set('useFindAndModify', false)
@@ -37,6 +39,9 @@ app.use(bodyParser.json())
 app.use('/api/users', users)
 app.use('/api/projects', projects)
 app.use('/api/tasks', tasks)
+app.use('/api/chats', chats)
+app.use('/api/messages/', messages)
+
 
 const port = process.env.PORT || 4000
 app.listen(port, () => { console.log(`server running on ${port}`) })
